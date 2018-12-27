77354681fd9f596798b84f1ed51e6bf52d415452
Morgan Diepart <morgandiepart@gmail.com>
2018-12-27 01:56:02 +0100

    Automatically creating folder "./data"

--- QtKfeteManager/KFeteManager/main.cpp
+++ QtKfeteManager/KFeteManager/main.cpp
@@ -1,9 +1,13 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QDir>
 
+#include "mainwindow.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    if(!QDir("./data").exists()){
+        QDir().mkdir("./data");
+    }
     MainWindow w;
     w.show();
 
