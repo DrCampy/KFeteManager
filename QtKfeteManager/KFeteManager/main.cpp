c8a6645d3c05d7f8709859370f4a343d9c6fbe43
Morgan Diepart <morgandiepart@gmail.com>
2018-12-27 18:56:40 +0100

    main file opens database and checks its integrity

--- QtKfeteManager/KFeteManager/main.cpp
+++ QtKfeteManager/KFeteManager/main.cpp
@@ -2,14 +2,22 @@
 #include <QDir>
 
 #include "mainwindow.h"
+#include "databasemanager.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     if(!QDir("./data").exists()){
         QDir().mkdir("./data");
     }
-    MainWindow w;
-    w.show();
+    //Opens database
+    DatabaseManager::openDatabase();
+
+    //Checks if database is OK
+    if(DatabaseManager::checkDatabase()){
+        //runs application
+        MainWindow w;
+        w.show();
+    }
 
     return a.exec();
 }
