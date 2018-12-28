d0760650bb7dbb2c1cf13072120a9ff83235a124
Morgan Diepart <morgandiepart@gmail.com>
2018-12-28 01:18:32 +0100

    Testing the database in main

--- QtKfeteManager/KFeteManager/main.cpp
+++ QtKfeteManager/KFeteManager/main.cpp
@@ -1,8 +1,10 @@
 #include <QApplication>
 #include <QDir>
+#include <QMessageBox>
 
 #include "mainwindow.h"
 #include "databasemanager.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -13,11 +15,30 @@ int main(int argc, char *argv[])
     DatabaseManager::openDatabase();
 
     //Checks if database is OK
-    if(DatabaseManager::checkDatabase()){
-        //runs application
-        MainWindow w;
-        w.show();
+    if(!DatabaseManager::checkDatabase()){
+        //If database not OK, proposes to create it.
+        QMessageBox ask;
+        ask.setText(QObject::tr("La base de donnée semble corrompue."));
+        ask.setInformativeText(QObject::tr("La recréer peut résoudre le problème mais peut causer la perte des données. La recréer ?"));
+        ask.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+        ask.setDefaultButton(QMessageBox::No);
+        int ret = ask.exec();
+        if(ret == QMessageBox::No){
+            return EXIT_FAILURE;
+        }
+        DatabaseManager::createDatabase();
+
     }
 
-    return a.exec();
+    //runs application
+    MainWindow w;
+    w.show();
+
+    //Executes application
+    int ret = a.exec();
+
+    //Closes Database
+    DatabaseManager::closeDatabase();
+
+    return ret;
 }
