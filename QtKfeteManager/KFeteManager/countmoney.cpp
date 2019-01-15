58d7dcb56be8675fa61700e7fa9ad22589f5b433
Morgan Diepart <morgandiepart@gmail.com>
2019-01-15 01:37:38 +0100

    Update countmoney

--- QtKfeteManager/KFeteManager/countmoney.cpp
+++ QtKfeteManager/KFeteManager/countmoney.cpp
@@ -3,13 +3,69 @@
 #include <QGridLayout>
 #include <QLabel>
 #include <QSpinBox>
+#include <QListWidget>
+#include <QListWidgetItem>
+#include <QPainter>
+#include <QComboBox>
+#include <QSqlQueryModel>
+#include <QDebug>
 
 #include "countmoney.h"
 #include "databasemanager.h"
 #include "clientlist.h"
 
-CountMoneyBefore::CountMoneyBefore(QWidget *parent) : QWidget(parent)
-{
+CountMoney::CountMoney(QWidget *parent) : QWidget(parent){
+    validate    = new QPushButton(tr("Valider"));
+    cancel      = new QPushButton(tr("Annuler"));
+    moneyForm   = new MoneyForm(this);
+}
+
+CountMoneyBefore::CountMoneyBefore(QWidget *parent) : CountMoney(parent){
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    QHBoxLayout *comboLayout = new QHBoxLayout();
+    QHBoxLayout *buttonsLayout = new QHBoxLayout();
+
+    buttonsLayout->addWidget(validate);
+    buttonsLayout->addWidget(cancel);
+    //Selecting jobists
+    //Display
+    jobistsList = new QListWidget(this);
+    jobistsList->setItemDelegate(new JobistDelegate(jobistsList));
+
+    //Selection
+    selectionCombo  = new QComboBox;
+    add             = new QPushButton(tr("Ajouter"), this);
+    comboLayout->addWidget(selectionCombo);
+    comboLayout->addWidget(add);
+
+    QSqlQueryModel *clientsModel  = new QSqlQueryModel(this);
+    clientsModel->setQuery("SELECT Name FROM Clients ORDER BY 'name' ASC;");
+    selectionCombo->setModel(clientsModel);
+
+    //validate/cancel buttons
+    mainLayout->addWidget(jobistsList);
+    mainLayout->addLayout(comboLayout);
+    mainLayout->addWidget(moneyForm);
+    mainLayout->addLayout(buttonsLayout);
+
+    connect(cancel, SIGNAL(clicked()), this, SIGNAL(cancelled()));
+    connect(add, SIGNAL(clicked()), this, SLOT(addJobist()));
+}
+
+void CountMoneyBefore::addJobist(){
+    QString name = this->selectionCombo->currentText();
+    QListWidgetItem *item = new QListWidgetItem;
+    item->setData(Qt::DisplayRole, QVariant::fromValue(JobistTag(name)));
+    jobistsList->addItem(item);
+}
+
+void CountMoneyBefore::removeJobist(QString name){
+    QListWidgetItem *item = new QListWidgetItem;
+    item->setData(Qt::DisplayRole, QVariant::fromValue(JobistTag(name)));
+    jobistsList->removeItemWidget(item);
+}
+
+CountMoneyAfter::CountMoneyAfter(QWidget *parent) : CountMoney(parent){
 
 }
 
@@ -21,9 +77,15 @@ MoneyForm::MoneyForm(QWidget *parent) : QWidget (parent)
     QString currency = DatabaseManager::getCurrency();
     QStringList notes = DatabaseManager::getNotes();
     QStringList coins = DatabaseManager::getCoins();
+
+    notesLabels = new QList<QLabel *>();
+    coinsLabels = new QList<QLabel *>();
     notesValues = new QVector<qreal>();
     coinsValues = new QVector<qreal>();
+    notesSpinBoxes = new QList<QSpinBox *>();
+    coinsSpinBoxes = new QList<QSpinBox *>();
 
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
     QGridLayout *gridNotes = new QGridLayout();
     QGridLayout *gridCoins = new QGridLayout();
 
@@ -54,6 +116,7 @@ MoneyForm::MoneyForm(QWidget *parent) : QWidget (parent)
     //layout the widgets
     //Notes
     gridNotes->addWidget(notesTitleLabel, 0, 0, 1, 1, Qt::AlignLeft|Qt::AlignBottom);
+    qDebug() << notesLabels->size();
     for(int i = 0; i < notesLabels->size(); i++){
         gridNotes->addWidget(notesLabels->at(i), 1+(i/4), (i%4)*2, 1, 1);
         gridNotes->addWidget(notesSpinBoxes->at(i), 1+(i/4), (i%4)*2+1, 1, 1);
@@ -64,6 +127,9 @@ MoneyForm::MoneyForm(QWidget *parent) : QWidget (parent)
         gridCoins->addWidget(coinsLabels->at(i), 1+(i/4), (i%4)*2, 1, 1);
         gridCoins->addWidget(coinsSpinBoxes->at(i), 1+(i/4), (i%4)*2+1, 1, 1);
     }
+
+    mainLayout->addLayout(gridCoins);
+    mainLayout->addLayout(gridNotes);
 }
 
 MoneyForm::~MoneyForm(){
@@ -90,4 +156,65 @@ qreal MoneyForm::getCount(){
     }
 
     return total;
+}
+
+JobistDelegate::JobistDelegate(QWidget *parent) : QStyledItemDelegate (parent){}
+
+void JobistDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const{
+    if(index.data().canConvert<JobistTag>()){
+        JobistTag jTag = qvariant_cast<JobistTag>(index.data());
+
+        if(option.state & QStyle::State_Selected){
+            painter->fillRect(option.rect, option.palette.highlight());
+        }
+
+        jTag.paint(painter, option);
+    }else{
+        QStyledItemDelegate::paint(painter, option, index);
+    }
+}
+
+JobistTag::JobistTag(QString name){
+    this->name = name;
+
+}
+
+void JobistTag::setName(QString name){
+    this->name = name;
+}
+
+void JobistTag::paint(QPainter *painter, const QStyleOptionViewItem &option) {
+
+    //Creates a painter and configure it
+    painter->save();
+
+    const int crossW = option.rect.height();
+    const int crossX = option.rect.right() - crossW;
+    const int crossY = option.rect.y();
+
+    if(option.state & QStyle::State_Selected){
+        painter->fillRect(option.rect, option.palette.highlight());
+    }
+    painter->setRenderHint(QPainter::Antialiasing, true);
+    //Sets color and styles. See Qt doc.
+    painter->setPen(QPen(Qt::red, 2, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin));
+
+    painter->setBrush(option.palette.foreground());
+
+    //Paint the cross
+    QLineF line1 = QLineF(1.464, 1.464, 8.535, 8.535);
+    QLineF line2 = QLineF(1.464, 8.535, 8.535, 1.464);
+
+    painter->scale(10/crossW, 10/crossW);
+    painter->translate(crossX, crossY);
+
+    painter->drawLine(line1);
+    painter->drawLine(line2);
+    painter->drawText(option.rect.left()+1, option.rect.bottom()+1, name);
+
+    painter->restore();
+}
+
+bool JobistTag::operator==(const JobistTag &tag) const{
+    return this->name == tag.name;
 }
--- QtKfeteManager/KFeteManager/countmoney.h
+++ QtKfeteManager/KFeteManager/countmoney.h
@@ -5,20 +5,60 @@
 #include <QLabel>
 #include <QSpinBox>
 #include <QList>
+#include <QStyledItemDelegate>
+#include <QComboBox>
+#include <QPushButton>
+#include <QListWidget>
 
-class CountMoneyBefore : public QWidget
+#include "clientlist.h"
+
+class JobistTag;
+class JobistDelegate;
+struct MoneyForm;
+
+class CountMoney : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit CountMoney(QWidget *parent = nullptr);
+
+protected:
+    MoneyForm *moneyForm;
+    QPushButton *validate;
+    QPushButton *cancel;
+
+signals:
+    void cancelled();
+
+};
+
+/*
+ * Interface that requests a list of jobists
+ * and the count of the cash register.
+ */
+class CountMoneyBefore : public CountMoney
 {
     Q_OBJECT
 public:
     explicit CountMoneyBefore(QWidget *parent = nullptr);
 
-signals:
+private:
+    QComboBox *selectionCombo;
+    QListWidget *jobistsList;
+    QList<JobistTag> *jobistsModel;
+    QPushButton *add;
 
-public slots:
+
+signals:
+    void validated(qreal, QList<Client>);
+
+private slots:
+    void addJobist();
+    void removeJobist(QString name);
 };
 
 
-class CountMoneyAfter: public QWidget
+class CountMoneyAfter: public CountMoney
 {
     Q_OBJECT
 public:
@@ -33,11 +73,12 @@ struct MoneyForm : public QWidget
 {
     Q_OBJECT
 
+public:
     explicit MoneyForm(QWidget *parent = nullptr);
     ~MoneyForm();
-
     qreal getCount();
 
+private:
     QList<QLabel *> *notesLabels;
     QList<QSpinBox *> *notesSpinBoxes;
 
@@ -48,5 +89,33 @@ struct MoneyForm : public QWidget
     QVector<qreal> *coinsValues;
 };
 
+//Item delegate to paint the names of the jobists in the list and also
+//Add a button that user have to click to remove the jobist.
+class JobistDelegate : public QStyledItemDelegate
+{
+    Q_OBJECT
+
+public:
+    explicit JobistDelegate(QWidget *parent = nullptr);
+
+    void paint(QPainter *painter, const QStyleOptionViewItem &option,
+               const QModelIndex &index) const;
+    //QSize sizeHint(const QStyleOptionViewItem &option,
+    //               const QModelIndex &index) const;
+};
+
+class JobistTag
+{
+
+private:
+    QString name;
+
+public:
+    explicit JobistTag(QString name = "");
+    void setName(QString name);
+    void paint(QPainter *painter, const QStyleOptionViewItem &option);
+    bool operator==(const JobistTag &tag) const;
+};
+Q_DECLARE_METATYPE(JobistTag)
 
 #endif // COUNTMONEY_H
