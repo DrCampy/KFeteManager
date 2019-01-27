39ece03d8be91fabd3a1e6859b03c7e017a4bb79
Morgan Diepart <morgandiepart@gmail.com>
2019-01-27 13:24:22 +0100

    Added creation script for history database

---
+++ QtKfeteManager/KFeteManager/data/scripts/create-history.sql
@@ -0,0 +1,56 @@
+--statement
+CREATE TABLE IF NOT EXISTS
+Clients(
+    Name TEXT PRIMARY KEY);
+
+--statement
+CREATE TABLE IF NOT EXISTS
+Sessions(
+    OpenTime INTEGER PRIMARY KEY,
+    closeTime INTEGER NOT NULL,
+    openAmount NUMERIC,
+    closeAmount NUMERIC,
+    incomes NUMERIC,
+    jobistShare NUMERIC,
+    jobistWage NUMERIC);
+
+--statement
+CREATE TABLE IF NOT EXISTS
+Jobists(
+    Session INT REFERENCES Sessions(OpenTime) ON DELETE CASCADE,
+    Client TEXT DEFAULT '****' REFERENCES Clients(Name) ON DELETE SET DEFAULT,
+    PRIMARY KEY(Session, Client));
+
+--statement
+CREATE TABLE IF NOT EXISTS
+FunctionsBenefits(
+    Session INT NOT NULL REFERENCES Sessions(OpenTime) ON DELETE CASCADE,
+    Function TEXT NOT NULL,
+    Amoun NUMERIC NOT NULL,
+    PRIMARY KEY(Session, Function));
+
+--statement
+CREATE TABLE IF NOT EXISTS
+AccountMoves(
+    Session INT REFERENCES Sessions(OpenTime) ON DELETE CASCADE,
+    Client TEXT REFERENCES Clients(Name),
+    Amount NUMERIC NOT NULL,
+    note TEXT);
+
+--statement
+CREATE TABLE IF NOT EXISTS
+CashMoves(
+    Session INT REFERENCES Sessions(OpenTime) ON DELETE CASCADE,
+    Amount NUMERIC NOT NULL,
+    note TEXT);
+
+--statement
+CREATE TABLE IF NOT EXISTS
+Sales(
+    Session INT REFERENCES Sessions(OpenTime) ON DELETE CASCADE,
+    Article TEXT NOT NULL,
+    quantity INTEGER NOT NULL CHECK (quantity > 0),
+    price TEXT NOT NULL CHECK(price IN('normal', 'reduced', 'free')));
+
+
+
