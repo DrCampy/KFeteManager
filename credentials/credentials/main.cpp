4829071976c16c515b3871ce26fbefc2ea654df2
Morgan Diepart <morgandiepart@gmail.com>
2019-04-26 19:25:45 +0200

    Creating credentials manager as sub-project

---
+++ credentials/credentials/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+
+using namespace std;
+
+/**
+ * @brief main: main function of the program.
+ * @param argc number of input arguments
+ * @param argv input arguments
+ * @return 0 if everything went well (check is OK), 1 otherwise.
+ *
+ *Input parameters :
+ * 1. File where the credentials are stored
+ * 2. The operation to perform
+ *    (ADD: Add a user-hash pair, UPDATE: update a hash, DELETE: delete a user-hash pair, CHECK: check a pair)
+ * 3. User
+ * 4. Hash
+ */
+int main(int argc, char *argv[])
+{
+    if(argc != 5){
+        fprintf(stderr, "Expecting 4 arguments but %d were received.", argc-1);
+        return 0;
+    }
+    string file = string(argv[1]);
+    string operation = string(argv[2]);
+    string user = string(argv[3]);
+    string hash = string(argv[4]);
+
+    if(operation.compare("ADD")){
+
+    }else if(operation.compare("UPDATE")){
+
+    }else if(operation.compare("DELETE")){
+
+    }else if(operation.compare("CHECK")){
+
+    }
+
+
+
+    return 0;
+}
+
